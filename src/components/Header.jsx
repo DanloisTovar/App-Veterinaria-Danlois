b0e3c2bd343211ea3dab1721a6c007486180b7dc
Danlois Tovar <danlois.tovar@gmail.com>
2022-10-21 22:58:58 -0300

    install tailwindcss.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Header() {
     return (
         <>
-            <h1>Header</h1>
+            <h1 className="text-4xl font-bold uppercase text-indigo-600 mt-5">
+                Header
+            </h1>
         </>
     );
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // !importar archivos enl que se va a utilizar tailwindcss (dentro del arreglo content:
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
