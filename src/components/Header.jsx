3ac8aeee2da745914ecb7109bbb984d070b7bc38
Danlois Tovar <danlois.tovar@gmail.com>
2022-10-27 19:45:42 -0300

    Update header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
             <h1 className="font-black text-5xl text-center md:w-1/2 mx-auto">
                 Seguimiento de pacientes{' '}
                 <span className=" text-indigo-600">
-                    Veterinaria DANLOIS TOVAR
+                    Veterinaria DANLOIS S.A.
                 </span>
             </h1>
             <p>
